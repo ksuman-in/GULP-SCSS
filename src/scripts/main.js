92834a7e56c6b57ceb9a125714cc9d925e4eaed4
sum21an <suman845415@gmail.com>
2019-09-30 20:57:54 +0530

    profile image changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,23 +13,11 @@ $(document).ready(function() {
 
     $(".tabs-infoblock").removeClass("active");
     $("#" + tabId + "-content").addClass("active");
-
-    // loader.show();
-    // footer.hide();
-    // header.find("li").css("pointer-events", "none");
-    //
-    // setTimeout(function() {
-    //   $("#" + tabId + "-content").addClass("active");
-    //   loader.hide();
-    //   footer.show();
-    //   header.find("li").css("pointer-events", "auto");
-    // }, 400);
-
     if (width > 767) {
       $("html, body").animate({ scrollTop: 0 }, "slow");
     } else {
       $("html, body").animate(
-        { scrollTop: $("#sidebar").width() + 82 },
+        { scrollTop: $("#sidebar").width() + 212 },
         "slow"
       );
     }
@@ -39,22 +27,24 @@ $(document).ready(function() {
       $(".react").animate({ width: "75%" }, 350);
       $(".redux").animate({ width: "70%" }, 350);
       $(".javascript").animate({ width: "75%" }, 350);
+      $(".sketch").animate({ width: "40%" }, 350);
       $(".wordpress").animate({ width: "80%" }, 350);
-      $(".bootstrap").animate({ width: "95%" }, 350);
+      $(".bootstrap").animate({ width: "85%" }, 350);
       $(".css").animate({ width: "80%" }, 350);
-      $(".html").animate({ width: "90%" }, 350);
+      $(".html").animate({ width: "85%" }, 350);
       $(".communication").animate({ width: "80%" }, 350);
       $(".confidence").animate({ width: "100%" }, 350);
     } else {
-      $(".react").animate({ width: "0%" }, 500);
-      $(".redux").animate({ width: "0%" }, 500);
-      $(".javascript").animate({ width: "0%" }, 500);
-      $(".wordpress").animate({ width: "0%" }, 500);
-      $(".bootstrap").animate({ width: "0%" }, 500);
-      $(".css").animate({ width: "0%" }, 500);
-      $(".html").animate({ width: "0%" }, 500);
-      $(".communication").animate({ width: "0%" }, 500);
-      $(".confidence").animate({ width: "0%" }, 500);
+      $(".react").animate({ width: "0%" });
+      $(".redux").animate({ width: "0%" });
+      $(".javascript").animate({ width: "0%" });
+      $(".sketch").animate({ width: "0%" });
+      $(".wordpress").animate({ width: "0%" });
+      $(".bootstrap").animate({ width: "0%" });
+      $(".css").animate({ width: "0%" });
+      $(".html").animate({ width: "0%" });
+      $(".communication").animate({ width: "0%" });
+      $(".confidence").animate({ width: "0%" });
     }
 
     event.preventDefault();
@@ -81,16 +71,4 @@ $(document).ready(function() {
   var m = year.getMonth();
   // alert(d+"/"+m+"/");
   $(".year").html(year.getFullYear());
-
-  $(".userName")
-    .on("mouseenter", function() {
-      $(this)
-        .text("Suman Kumar")
-        .addClass("hover-name");
-    })
-    .on("mouseleave", function() {
-      $(this)
-        .text("Kumar Suman")
-        .removeClass("hover-name");
-    });
 });
